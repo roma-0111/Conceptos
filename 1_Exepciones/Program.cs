070a6e75f1ec96d6ba4b9a0a68a6870691d32ac7
Xia <rbrian.0111@gmail.com>
2026-01-18 01:19:51 -0300

    Initial commit

---
+++ 1_Exepciones/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1_Exepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos a las variables.
+            int zero = 0, uno = 1;
+
+            //Creamos una exepción dentro del try a propósito.
+            try
+            {
+                int num1 = uno / zero;
+            }
+            //En caso de haber una excepción se ejecuta el catch
+            catch (Exception e) //Exception: Al no saber cuál exepción es, se utiliza "Exception" para ser general.
+            {
+                //Mostramos un mensaje que no se hizo la operación y mostramos la exepción que ocurre.
+                Console.WriteLine("No se puede realizar esa operación !!!");
+                Console.WriteLine($"El error es: {e}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
