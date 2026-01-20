5802a1d94690a827bae452afaa96d4de9e0ed983
Xia <rbrian.0111@gmail.com>
2026-01-20 10:47:19 -0300

    Modificación general
    Hola, este cambio se agregan comentarios y se agrega un finally para que si o si pase una acción sin importar que esté el o no la exepción. :D

--- 1_Exepciones/Program.cs
+++ 1_Exepciones/Program.cs
@@ -10,6 +10,7 @@
             //Creamos una exepción dentro del try a propósito.
             try
             {
+                //En el try es siempre que donde tiene que ir la posible exepción.
                 int num1 = uno / zero;
             }
             //En caso de haber una excepción se ejecuta el catch
@@ -18,8 +19,12 @@
                 //Mostramos un mensaje que no se hizo la operación y mostramos la exepción que ocurre.
                 Console.WriteLine("No se puede realizar esa operación !!!");
                 Console.WriteLine($"El error es: {e}");
-
-                Console.ReadKey();
+            }
+            finally
+            {
+                //Para esperar y salir
+                Console.WriteLine("Presione cualquier tecla para salir.");
+                Console.ReadKey(true);
             }
         }
     }
